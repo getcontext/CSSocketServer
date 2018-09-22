6c89709303407ffca0a0bd546e815d4604d0ca67
getcontext <andrzej.salamon@gmail.com>
2018-09-22 21:50:35 +0200

    initial commit

---
+++ Main.cs
@@ -0,0 +1,10 @@
+namespace cssocketserver
+{
+    class Main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("andresko (web)socket server 1.0");
+        }
+    }
+}
